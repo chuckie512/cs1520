48f97f44129a3b0c32ecf9397c79b9cd8673488f
Charles Smith <cas275@pitt.edu>
2016-03-23 18:35:38 -0400

    started bonus

---
+++ bonus1/bonus_one.js
@@ -0,0 +1,34 @@
+// The anonymous function below will fire on page load
+
+// Some things to consider
+// $.ajax(); to make your requests a little easier. Or the vanilla js way, it's up to you.
+// $.on(); for event handling
+// Remember, selecting elements in jQuery is like selecting them in CSS
+// You'll probably have to manipulate some strings
+// some jQuery functions to help display results
+// $.show(), $.hide(), $.slideup(), $.slidedown(), $.fadein(), $.fadeout()
+// Add content from requests with something like
+// $.html(), $.text(), etc.
+// keyup events could be helpful to get value of field as the user types
+
+function magic() {
+	// Magic!
+	console.log('Keepin\'n it clean with an external script!');
+    var xhttp = new XMLHttpRequest();
+    var type = "all";
+    xhttp.onreadystatechange = function(){
+        var test = xhttp.responseText;
+        test.trim();
+        console.log(test);
+        var response = JSON.parse(test);
+        console.log(response);
+        console.log(response.data.interests);
+
+        var array = response.data.interests.concat(response.data.programming);
+        //jquery auto-complete on the id=search
+        $("#search").autocomplete({source: array})
+
+    };
+    xhttp.open("GET", "http://www.mattbowytz.com/simple_api.json?data=" + type, true);
+    xhttp.send();
+}
