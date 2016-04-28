88a537c766e3c7f7ed06dc0d55182aaec75ca3c9
Charles Smith <cas275@pitt.edu>
2016-04-28 12:05:45 -0400

    changes to ex4

--- exercise4/home.php
+++ exercise4/home.php
@@ -1,40 +1,42 @@
 <?php
-    session_start();
-    if(!isset($_SESSION["name"])){
-        $_SESSION["error"] = "<p class='error'>You are not logged in!<br/>Please log in below</p>";
-        header('location: '.'ex4.php');
-        die();
-    }
+session_start();
+if(!isset($_SESSION["name"])){
+  $_SESSION["error"] = "<p class='error'>You are not logged in!<br/>Please log in below</p>";
+  header('location: '.'ex4.php');
+  die();
+}
 
-    $name = $_SESSION["name"];
+$name = $_SESSION["name"];
 ?>
 <!DOCTYPE html>
 <html>
-    <head>
-        <style>
-            body{
-                margin: 2%;
-            }
-            footer{
-                font-size: 6pt;
-                color: grey;
-                text-align: center;
-                text-wrap: normal;
-                margin: 5px;
-                bottom: 0;
-                position: fixed;
-                width: 100%;
-            }
-        </style>
-    </head>
-    <body>
-        <?php
-            echo "Welcome, ".$name;
-        ?>
-        <br/>
-        <a href="ex4.php">Logout</a>
-        <footer>
-            Created by Charles Smith &ltcas275@pitt.edu&gt for CS 1520 spring 2016
-        </footer>
-    </body>
+<head>
+  <style>
+    body{
+      margin: 2%;
+      border: solid blue 2px;
+      border-radius: 5px;
+    }
+    footer{
+      font-size: 6pt;
+      color: grey;
+      text-align: center;
+      text-wrap: normal;
+      margin: 5px;
+      bottom: 0;
+      position: fixed;
+      width: 100%;
+    }
+  </style>
+</head>
+<body>
+  <?php
+  echo "Welcome, ".$name;
+  ?>
+  <br/>
+  <a href="ex4.php">Logout</a>
+  <footer>
+    Created by Charles Smith &ltcas275@pitt.edu&gt for CS 1520 spring 2016
+  </footer>
+</body>
 </html>
