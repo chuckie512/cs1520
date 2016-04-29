b2d2f0770124f8e84c9502b5b04bfd47c2de59e6
Charles Smith <cas275@pitt.edu>
2016-04-29 19:34:28 -0400

    ex9

---
+++ exercise9/ex9.js
@@ -0,0 +1,44 @@
+/**
+ * Created by cas275 on 4/29/16.
+ */
+
+var words = [];
+
+
+$(document).ready(function(){
+  $("#button").click(function(){
+    getWord();
+  });
+
+});
+
+function getWord(){
+  $.get("getWords.php", function(data){
+    var word = $(data).find("value").text();
+
+    var i = 0;
+    var found = false;
+    for(i=0; i<words.length; i++){
+      if(words[i].name == word){
+        words[i].val++;
+        found = true;
+      }
+    }
+    if(!found){
+      words.push({
+        name: word,
+        val: 1
+      });
+    }
+  });
+  updateTable();
+}
+
+function updateTable(){
+  var tableString = "";
+  var j = 0;
+  for(j=0; j<words.length; j++){
+    tableString += " <tr> <td>" + words[j].name + "</td><td>"+words[j].val+"</td></tr> ";
+  }
+  $(".thetable").html(tableString);
+}
